fb2decda06914bcd862cc4827ed4d2308730da66
justfaruqinnit <sanusifaruq007@gmail.com>
2026-03-08 20:18:52 +0100

    fixed 500 interval server error

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -6,8 +6,8 @@ import cron from 'node-cron'
 import { createClient } from '@supabase/supabase-js'
 
 const PORT = Number(process.env.PORT ?? 8787)
-const API_URL = process.env.VITE_API_URL ?? 'https://quicktasks-backend-wqb3.onrender.com'
-const APP_URL = process.env.APP_URL ?? 'https://quick-tasks-eight.vercel.app/'
+const API_URL = process.env.VITE_API_URL ?? 'https://quicktasks-backend-ym28.onrender.com/'
+const APP_URL = process.env.APP_URL ?? 'http://localhost:5173/'
 const SUPABASE_URL = process.env.SUPABASE_URL
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY
 
