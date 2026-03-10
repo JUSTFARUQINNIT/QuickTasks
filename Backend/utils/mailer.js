af61a6c93412ecae7eea7066651b5b626e01b646
justfaruqinnit <sanusifaruq007@gmail.com>
2026-03-10 11:38:56 +0100

    fixed email sending by reconfiguring the smtp server

--- Backend/utils/mailer.js
+++ Backend/utils/mailer.js
@@ -1,16 +1,11 @@
-import nodemailer from "nodemailer"
-
-const mailUser = process.env.EMAIL_USER
-const mailPass = process.env.EMAIL_PASS
+import nodemailer from "nodemailer";
 
 export const mailTransport = nodemailer.createTransport({
-  service: "gmail",
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4,
+  port: 465,
+  secure: true, // SSL
   auth: {
-    user: mailUser,
-    pass: mailPass
-  }
-})
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
